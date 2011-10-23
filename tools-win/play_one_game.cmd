bf563215aff2fb01a91130db365f1be64a6e7b3b
Bill <github@billyang.me>
2011-10-23 15:38:44 -0600

    v2 - added combat logic, improved other logic

--- tools-win/play_one_game.cmd
+++ tools-win/play_one_game.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-python "%~dp0playgame.py" --engine_seed 42 --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 100 --map_file "%~dp0maps\random_walk\random_walk_04p_01.map" %* "python3 ..\src\MyBot.py3" "python sample_bots\RandomBot.py" "python ""%~dp0sample_bots\python\HunterBot.py""" "python ""%~dp0sample_bots\python\GreedyBot.py"""
+python "%~dp0playgame.py" --engine_seed 42 --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 500 --map_file "%~dp0maps\random_walk\random_walk_04p_01.map" %* "python3 ..\src\MyBot.py3" "python ""%~dp0sample_bots\python\HunterBot.py""" "python ""%~dp0sample_bots\python\HunterBot.py""" "python ""%~dp0sample_bots\python\GreedyBot.py"""
 
--- tools-win/play_one_game_live.cmd
+++ tools-win/play_one_game_live.cmd
@@ -1,3 +1,3 @@
 @echo off
-python "%~dp0playgame.py" -So --engine_seed 42 --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file "%~dp0maps\maze\maze_04p_01.map" %* "python ""%~dp0sample_bots\python\HunterBot.py""" "python ""%~dp0sample_bots\python\LeftyBot.py""" "python ""%~dp0sample_bots\python\HunterBot.py""" "python ""%~dp0sample_bots\python\LeftyBot.py""" | java -jar visualizer.jar
+python "%~dp0playgame.py" -So --engine_seed 42 --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file "%~dp0maps\random_walk\random_walk_04p_01.map" %* "python3 ..\src\MyBot.py3" "python ""%~dp0sample_bots\python\HunterBot.py""" "python ""%~dp0sample_bots\python\GreedyBot.py""" "python ""%~dp0sample_bots\python\HunterBot.py""" | java -jar visualizer.jar
 

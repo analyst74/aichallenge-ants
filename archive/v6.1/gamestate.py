9eb1d7d892b63da6df7fb2995679443876a3b055
Bill <github@billyang.me>
2011-11-15 23:56:32 -0700

    6.1 folder

---
+++ archive/v6.1/gamestate.py
@@ -0,0 +1,374 @@
+# gamestate.py: stuff related to game state management, including I/O with game engine
+#
+# AI bot written for AI Challenge 2011 - Ants
+# Author: Bill Y
+# License: all your base are belong to us
+
+import sys, random, time, numpy as np
+from core import *
+
+PLAYER_ANT = 'abcdefghij'
+HILL_ANT = string = 'ABCDEFGHI'
+PLAYER_HILL = string = '0123456789'
+MAP_OBJECT = '?%*.!'
+MAP_RENDER = PLAYER_ANT + HILL_ANT + PLAYER_HILL + MAP_OBJECT
+
+          
+class GameState():
+    def __init__(self):
+        self.cols = None
+        self.rows = None
+        self.map = None
+        #self.strat_map = None # simplified nodes of map
+        self.hill_list = {}
+        self.ant_list = {}
+        self.my_fighters = []
+        self.water_list = {}
+        self.dead_list = defaultdict(list)
+        self.food_list = []
+        self.turntime = 0
+        self.loadtime = 0
+        self.turn_start_time = None
+        self.vision = None
+        self.viewradius2 = 0
+        self.attackradius2 = 0
+        self.spawnradius2 = 0
+        self.turns = 0
+        self.current_turn = 0
+        self.sqrt_table = {}
+        self.neighbour_table = {}
+        self.uc_distance_table = {}
+        self.winning_percentage = 0.0
+
+    def setup(self, data):
+        'parse initial input and setup starting game state'
+        for line in data.split('\n'):
+            line = line.strip().lower()
+            if len(line) > 0:
+                tokens = line.split()
+                key = tokens[0]
+                if key == 'cols':
+                    self.cols = int(tokens[1])
+                elif key == 'rows':
+                    self.rows = int(tokens[1])
+                elif key == 'player_seed':
+                    random.seed(int(tokens[1]))
+                elif key == 'turntime':
+                    self.turntime = int(tokens[1])
+                elif key == 'loadtime':
+                    self.loadtime = int(tokens[1])
+                elif key == 'viewradius2':
+                    self.viewradius2 = int(tokens[1])
+                elif key == 'attackradius2':
+                    self.attackradius2 = int(tokens[1])
+                elif key == 'spawnradius2':
+                    self.spawnradius2 = int(tokens[1])
+                elif key == 'turns':
+                    self.turns = int(tokens[1])
+        self.map = np.array([[LAND for col in range(self.cols)]
+                    for row in range(self.rows)])
+        #self.strat_map = np.ones((int(self.rows/EXPLORE_GAP), int(self.cols/EXPLORE_GAP)))
+                    
+        # setup neighbour table
+        self.neighbour_table = {(row,col):self.get_neighbour_locs((row,col)) 
+                                for row in xrange(self.rows) 
+                                for col in xrange(self.cols)}
+                            
+    def update(self, data):
+        'parse engine input and update the game state'        
+        # start timer
+        self.turn_start_time = time.time()
+        
+        # reset vision
+        self.vision = None
+        
+        # hill is slightly different, we do want to remember 
+        # where hills are, so we know where to attack
+        #self.hill_list = {(row, col):self.hill_list[(row, col)] for (row, col) in self.hill_list 
+        #                if self.map[row][col] == HILL}
+
+        # clear ant and food data
+        for row, col in self.ant_list.keys():
+            self.map[row][col] = LAND
+        self.ant_list = {}
+        
+        for row, col in self.dead_list.keys():
+            self.map[row][col] = LAND            
+        self.dead_list = defaultdict(list)
+        
+        for row, col in self.food_list:
+            self.map[row][col] = LAND            
+        self.food_list = []
+        
+        # update map and create new ant and food lists
+        for line in data.split('\n'):
+            line = line.strip().lower()
+            if len(line) > 0:
+                tokens = line.split()
+                if len(tokens) == 2:
+                    # log current turn
+                    self.current_turn = tokens[1]
+                elif len(tokens) >= 3 and len(tokens[0]) == 1:
+                    row = int(tokens[1])
+                    col = int(tokens[2])
+                    if tokens[0] == 'w':
+                        self.map[row][col] = WATER
+                        self.water_list[(row, col)] = 1
+                    elif tokens[0] == 'f':
+                        self.map[row][col] = FOOD
+                        self.food_list.append((row, col))
+                    else:
+                        owner = int(tokens[3])
+                        if tokens[0] == 'a':
+                            # a hill got razed
+                            if (row,col) in self.hill_list and owner != self.hill_list[(row,col)]:
+                                del(self.hill_list[(row,col)])
+                            self.map[row][col] = owner
+                            self.ant_list[(row, col)] = (owner, False)
+                        elif tokens[0] == 'd':
+                            # food could spawn on a spot where an ant just died
+                            # don't overwrite the space unless it is land
+                            if self.map[row][col] == LAND:
+                                self.map[row][col] = DEAD
+                            # but always add to the dead list
+                            self.dead_list[(row, col)].append(owner)
+                        elif tokens[0] == 'h':
+                            owner = int(tokens[3])
+                            self.map[row][col] = HILL
+                            self.hill_list[(row, col)] = owner
+        # for row in range(self.strat_map.shape[0]):
+            # for col in range(self.strat_map.shape[1]):
+                # if self.visible((row*EXPLORE_GAP, col*EXPLORE_GAP)):
+                    # self.strat_map[row,col] = 0
+        
+    def time_remaining(self):
+        return self.turntime - self.time_elapsed()
+    
+    def time_elapsed(self):
+        return int(1000 * (time.time() - self.turn_start_time))
+            
+    def move_toward(self, ant_loc, target_loc):
+        'move toward the generao direction'
+        possible_moves = [ant_loc] + self.passable_neighbours(ant_loc)
+        move_distances = [self.manhattan_distance(move, target_loc) for move in possible_moves]
+        
+        best_distance, best_move = sorted(zip(move_distances, possible_moves))[0]
+        directions = self.direction(ant_loc, best_move)
+        if len(directions) > 0:
+            self.issue_order((ant_loc, directions[0]))
+    
+    def issue_order(self, order):
+        'issue an order by writing the proper ant location and direction'
+        (row, col), direction = order
+        #early exit
+        if direction is None:
+            self.ant_list[(row,col)] = (MY_ANT, True)
+            #logging.debug('moving %s -- stationary' % str((row, col)))
+            return
+        
+        (newrow, newcol) = self.destination((row, col), direction)
+        if self.is_passable((newrow, newcol)):
+            sys.stdout.write('o %s %s %s\n' % (row, col, direction))
+            sys.stdout.flush()
+            #logging.debug('moving %s to %s' % (str((row, col)), str((newrow, newcol))))
+            # update ant moved flag
+            del self.ant_list[(row,col)]
+            self.ant_list[(newrow, newcol)] = (MY_ANT, True)
+            # update map info (to avoid to ants moving into the same spot)
+            self.map[newrow][newcol] = MY_ANT
+            self.map[row][col] = LAND
+        else:
+            logging.debug('INVALID order: %s' % str(order))
+    
+    def finish_turn(self):
+        'finish the turn by writing the go line'
+        sys.stdout.write('go\n')
+        sys.stdout.flush()
+    
+    def my_hills(self):
+        return [loc for loc, owner in self.hill_list.items()
+                    if owner == MY_ANT]
+
+    def enemy_hills(self):
+        return [(loc, owner) for loc, owner in self.hill_list.items()
+                    if owner != MY_ANT]
+        
+    def my_ants(self):
+        'return a list of all my ants'
+        return [(row, col) for (row, col), (owner, moved) in self.ant_list.items()
+                    if owner == MY_ANT]
+
+    def my_unmoved_ants(self):
+        'return a list of un-moved ants'
+        return [(row, col) for (row, col), (owner, moved) in self.ant_list.items()
+                    if owner == MY_ANT and not moved]
+                    
+    def enemy_ants(self):
+        'return a list of all visible enemy ants'
+        return [((row, col), owner)
+                    for (row, col), (owner, moved) in self.ant_list.items()
+                    if owner != MY_ANT]
+               
+    def food(self):
+        'return a list of all food locations'
+        return self.food_list[:]
+    
+    def is_water(self, loc):
+        (row, col) = loc
+        return self.map[row][col] == WATER
+        
+    def is_unoccupied(self, loc):
+        'true if no ants are at the location'
+        row, col = loc
+        return self.map[row][col] in (LAND, DEAD)
+
+    def is_my_unmoved_ant(self, loc):
+        'true if is my ant, and is not moved'
+        return loc in self.ant_list and self.ant_list[loc][0] == MY_ANT and self.ant_list[loc][1] == False
+    
+    def is_passable(self, loc):
+        'true if not water or ant or food'
+        row, col = loc
+        return self.map[row][col] in (LAND, DEAD, HILL)
+        
+    def is_passable_override(self, loc, unpassable_override, passable_override):
+        'instead of simple passable, override with additional unpassable and passable locations'
+        'note that unpassable_override takes precedence over passable_override'
+        if loc in unpassable_override:
+            return False
+        elif loc in passable_override:
+            return True
+        else:
+            return self.is_passable(loc)
+        
+    def is_passable_ignore_ant(self, loc):
+        'true if not water or food'
+        row, col = loc
+        return self.map[row][col] not in (WATER, FOOD)
+        
+    def passable_directions(self, loc):
+        'finds valid move from given location, based on passable'
+        passable_directions = []
+        for direction in ALL_DIRECTIONS:
+            new_loc = self.destination(loc, direction)
+            if (self.is_passable(new_loc)):
+                passable_directions.append(direction)
+                
+        return passable_directions
+
+    def passable_neighbours(self, loc):
+        return [l for l in self.neighbour_table[loc] if self.is_passable(l)]
+        
+    def destination(self, loc, direction):
+        'calculate a new location given the direction and wrap correctly'
+        row, col = loc
+        d_row, d_col = AIM[direction]
+        return ((row + d_row) % self.rows, (col + d_col) % self.cols)
+
+    def direction_row(self, loc, direction, distance):
+        'get all tiles in given direction for given distance'
+        row, col = loc
+        d_row, d_col = AIM[direction]
+        return [((row + d_row * i) % self.rows, (col + d_col*i) % self.cols) for i in xrange(distance)]
+        
+    def manhattan_distance(self, loc1, loc2):
+        'calculate the manhattan distance between to locations'
+        row1, col1 = loc1
+        row2, col2 = loc2
+        d_col = min(abs(col1 - col2), self.cols - abs(col1 - col2))
+        d_row = min(abs(row1 - row2), self.rows - abs(row1 - row2))
+        return d_row + d_col
+
+    def euclidean_distance2(self, loc1, loc2):
+        'calculate the euclidean distance between to locations'
+        
+        # if (loc1, loc2) not in self.uc_distance_table:
+            # row1, col1 = loc1
+            # row2, col2 = loc2
+            # d_col = min(abs(col1 - col2), self.cols - abs(col1 - col2))
+            # d_row = min(abs(row1 - row2), self.rows - abs(row1 - row2))
+            # self.uc_distance_table[(loc1, loc2)] = d_row**2 + d_col**2
+            
+        # return self.uc_distance_table[(loc1, loc2)]
+        
+        row1, col1 = loc1
+        row2, col2 = loc2
+        d_col = min(abs(col1 - col2), self.cols - abs(col1 - col2))
+        d_row = min(abs(row1 - row2), self.rows - abs(row1 - row2))
+        return d_row**2 + d_col**2
+        
+    def euclidean_distance_add(self, distance2, addition):
+        'do euclidean math.add'
+        distance = self.get_sqrt(distance2)
+        return (distance+addition)**2
+
+    def get_sqrt(self, num):
+        if num not in self.sqrt_table:
+            self.sqrt_table[num] = sqrt(num)
+        return self.sqrt_table[num]
+            
+    def direction(self, loc1, loc2):
+        'determine the 1 or 2 fastest (closest) directions to reach a location'
+        row1, col1 = loc1
+        row2, col2 = loc2
+        height2 = self.rows//2
+        width2 = self.cols//2
+        d = []
+        if row1 < row2:
+            if row2 - row1 >= height2:
+                d.append('n')
+            if row2 - row1 <= height2:
+                d.append('s')
+        if row2 < row1:
+            if row1 - row2 >= height2:
+                d.append('s')
+            if row1 - row2 <= height2:
+                d.append('n')
+        if col1 < col2:
+            if col2 - col1 >= width2:
+                d.append('w')
+            if col2 - col1 <= width2:
+                d.append('e')
+        if col2 < col1:
+            if col1 - col2 >= width2:
+                d.append('e')
+            if col1 - col2 <= width2:
+                d.append('w')
+        return d        
+
+    def visible(self, loc):
+        ' determine which squares are visible to the given player '
+
+        if self.vision == None:
+            if not hasattr(self, 'vision_offsets_2'):
+                # precalculate squares around an ant to set as visible
+                self.vision_offsets_2 = []
+                mx = int(sqrt(self.viewradius2))
+                for d_row in range(-mx,mx+1):
+                    for d_col in range(-mx,mx+1):
+                        d = d_row**2 + d_col**2
+                        if d <= self.viewradius2:
+                            self.vision_offsets_2.append((
+                                d_row%self.rows-self.rows,
+                                d_col%self.cols-self.cols
+                            ))
+            # set all spaces as not visible
+            # loop through ants and set all squares around ant as visible
+            self.vision = [[False]*self.cols for row in range(self.rows)]
+            for ant in self.my_ants():
+                a_row, a_col = ant
+                for v_row, v_col in self.vision_offsets_2:
+                    self.vision[a_row+v_row][a_col+v_col] = True
+        row, col = loc
+        return self.vision[row][col]
+        
+    def get_neighbour_locs(self, loc):
+        'get all neighbour locations'
+        return [self.destination(loc, direction) for direction in ALL_DIRECTIONS]
+ 
+    def render_text_map(self):
+        'return a pretty string representing the map'
+        tmp = ''
+        for row in self.map:
+            tmp += '# %s\n' % ''.join([MAP_RENDER[col] for col in row])
+        return tmp
